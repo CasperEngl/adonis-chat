79f2f403998b3d3a8d283e36bfd61a667467650a
Casper Engelmann <me@casperengelmann.com>
2018-11-21 15:04:43 +0100

    Refractored Message to stateless component

--- resources/assets/js/components/Message/index.jsx
+++ resources/assets/js/components/Message/index.jsx
@@ -1,9 +1,15 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Row, Col, Card, CardText, Tooltip,
+  Row,
+  Col,
+  Card,
+  CardTitle,
+  CardText,
+  UncontrolledTooltip,
 } from 'reactstrap';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 
 const StyledCardText = styled(CardText)`
   width: 100%;
@@ -14,90 +20,90 @@ const StyledCard = styled(Card)`
   max-width: 100%;
 `;
 
-class Message extends PureComponent {
-  static propTypes = {
-    id: PropTypes.number.isRequired,
-    content: PropTypes.string.isRequired,
-    sender: PropTypes.bool.isRequired,
-    createdAt: PropTypes.string.isRequired,
-  }
+const Message = ({
+  id,
+  content,
+  createdAt,
+  users,
+  user,
+  sender,
+}) => {
+  const title = new Date(createdAt).toLocaleTimeString();
+  const target = `message_${id}`;
 
-  constructor(props) {
-    super(props);
+  const messageUser = users.find(u => u.id === user);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      tooltipOpen: false,
-    };
-  }
-
-  toggle() {
-    this.setState(prevState => ({
-      tooltipOpen: !prevState.tooltipOpen,
-    }));
-  }
-
-  render() {
-    const {
-      id,
-      content,
-      sender,
-      createdAt,
-    } = this.props;
-    const { tooltipOpen } = this.state;
-
-    const title = new Date(createdAt).toLocaleTimeString();
-    const target = `message_${id}`;
-
-    if (sender) {
-      return (
-        <Row className="my-2 align-items-center justify-content-end">
-          <Col xs="8" className="d-flex flex-column align-items-end">
-            <StyledCard body inverse color="primary" className="d-flex align-items-end" id={target}>
-              <StyledCardText>{ content }</StyledCardText>
-            </StyledCard>
-            <Tooltip
-              placement="auto"
-              isOpen={tooltipOpen}
-              target={target}
-              toggle={this.toggle}
-              delay={{
-                show: 500,
-                hide: 0,
-              }}
-            >
-              { title }
-            </Tooltip>
-          </Col>
-        </Row>
-      );
-    }
-
+  if (sender) {
     return (
-      <Row className="my-2 align-items-center">
-        <Col tag="figure" sm="auto" className="pr-0">
-          <img src="/img/avatar.png" alt="avatar" height="40" className="conversation__avatar" />
-        </Col>
-        <Col sm="8">
-          <StyledCard body id={`message_${id}`}>
+      <Row className="my-2 align-items-center justify-content-end">
+        <Col xs="8" className="d-flex flex-column align-items-end">
+          <StyledCard body inverse color="primary" className="d-flex align-items-end" id={target}>
             <StyledCardText>{ content }</StyledCardText>
           </StyledCard>
-          <Tooltip
+          <UncontrolledTooltip
             placement="auto"
-            isOpen={tooltipOpen}
             target={target}
-            toggle={this.toggle}
             delay={{
               show: 500,
               hide: 0,
             }}
           >
             { title }
-          </Tooltip>
+          </UncontrolledTooltip>
         </Col>
       </Row>
     );
   }
-}
 
-export default Message;
+  return (
+    <Row className="my-2 align-items-center">
+      <Col tag="figure" sm="auto" className="pr-0">
+        <img src="/img/avatar.png" alt="avatar" height="40" className="conversation__avatar" />
+      </Col>
+      <Col sm="8">
+        <StyledCard body id={`message_${id}`}>
+          {
+            messageUser && (
+              <CardTitle className="small">
+                { messageUser.firstName }
+                {' '}
+                { messageUser.lastName }
+              </CardTitle>
+            )
+          }
+          <StyledCardText>{ content }</StyledCardText>
+        </StyledCard>
+        <UncontrolledTooltip
+          placement="auto"
+          target={target}
+          delay={{
+            show: 500,
+            hide: 0,
+          }}
+        >
+          { title }
+        </UncontrolledTooltip>
+      </Col>
+    </Row>
+  );
+};
+
+Message.propTypes = {
+  id: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+  users: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  })).isRequired,
+  user: PropTypes.number.isRequired,
+  createdAt: PropTypes.string.isRequired,
+  sender: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+  users: state.conversation.current.users,
+});
+
+export default connect(mapStateToProps)(Message);
